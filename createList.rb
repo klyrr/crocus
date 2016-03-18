e9adc7ac2ede9fa22538a1f21f549b2d5e96cf71
Tina Umlandt <tina.umlandt@jimdo.com>
2016-03-18 13:49:00 +0100

    initial commits to create the currency.json list

---
+++ createList.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+
+CURRENCY_KEY = 'Ccy'
+CURRENCY_UNIT = 'CcyMnrUnts'
+CURRENCY_JSON_FILE_NAME = 'currency.json'
+
+currencyCode = {}
+
+doc = Nokogiri::XML(open("http://www.currency-iso.org/dam/downloads/lists/list_one.xml"))
+
+doc.xpath('//CcyNtry').each do |e|
+  if !e.at_xpath(CURRENCY_KEY).nil?
+    if !e.at_xpath(CURRENCY_UNIT).nil?
+      currency = e.at_xpath(CURRENCY_KEY).content()
+      next if currency.start_with?('X')
+
+      units = e.at_xpath(CURRENCY_UNIT).content()
+      currencyCode[currency] = {
+        'decimal' => units,
+        'symbol' => currency
+      }
+    end
+  end
+end
+
+File.write(CURRENCY_JSON_FILE_NAME, currencyCode.to_json)
