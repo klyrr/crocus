2ad6f02ef209afeb7dd3c97b2e98664671eb10a5
Tina Umlandt <tina.umlandt@jimdo.com>
2016-05-27 12:11:14 +0200

    fix: use numbers in tests

--- test/crocusSpec.js
+++ test/crocusSpec.js
@@ -8,9 +8,9 @@ module.exports = {
       group_sep: '.',
     };
 
-    assert.equal(crocus.formatNumber('10000', formatPattern), '10.000,00');
-    assert.equal(crocus.formatNumber('200000000', formatPattern), '200.000.000,00');
-    assert.equal(crocus.formatNumber('432.55', formatPattern), '432,55');
+    assert.equal(crocus.formatNumber(10000, formatPattern), '10.000,00');
+    assert.equal(crocus.formatNumber(200000000, formatPattern), '200.000.000,00');
+    assert.equal(crocus.formatNumber(432.55, formatPattern), '432,55');
   },
   'test crocus format currency': function(beforeExit, assert) {
     const formatCurrencyPattern = {
@@ -20,7 +20,7 @@ module.exports = {
       symbol: '€',
     };
 
-    assert.equal(crocus.formatCurrency('10000', formatCurrencyPattern), '10.000,00 €');
+    assert.equal(crocus.formatCurrency(10000, formatCurrencyPattern), '10.000,00 €');
   },
   'test crocus format currency for USD': function(beforeExit, assert) {
     const formatCurrencyPatternForUSD = {
@@ -30,7 +30,7 @@ module.exports = {
       symbol: '$',
     };
 
-    assert.equal(crocus.formatCurrency('10000000', formatCurrencyPatternForUSD), '$10,000,000.00');
+    assert.equal(crocus.formatCurrency(10000000, formatCurrencyPatternForUSD), '$10,000,000.00');
   },
   'test crocus format currency for negative and positive numbers': function(beforeExit, assert) {
     const formatCurrencyPattern = {
@@ -40,8 +40,8 @@ module.exports = {
       symbol: '€',
     };
 
-    assert.equal(crocus.formatCurrency('10000', formatCurrencyPattern), '10.000,00 €');
-    assert.equal(crocus.formatCurrency('-10000', formatCurrencyPattern), '-10.000,00 €');
+    assert.equal(crocus.formatCurrency(10000, formatCurrencyPattern), '10.000,00 €');
+    assert.equal(crocus.formatCurrency(-10000, formatCurrencyPattern), '-10.000,00 €');
   },
   'test crocus format not currency': function(beforeExit, assert) {
     const wrongFormatCurrencyPattern = {
@@ -52,7 +52,7 @@ module.exports = {
     };
 
     try {
-      const notDefined = crocus.formatCurrency('10000', wrongFormatCurrencyPattern);
+      const notDefined = crocus.formatCurrency(10000, wrongFormatCurrencyPattern);
       assert.equal(notDefined, 'This must not happen');
     } catch(e) {
       assert.equal(e, 'Given format is wrong');
